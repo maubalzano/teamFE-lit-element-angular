a9a949b49ca3abbbf1ae5c10a34f4a36fce9d261
maubalzano <m.balzano@eco-mind.eu>
2022-07-13 12:55:35 +0200

    web component integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'teamfe-lit-user-card';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'teamfe-lit-angular';
+
+  public user: User = {
+    firstName: "Francesco",
+    lastName: "Rossi",
+    dob: '22-09-2022',
+    gender: 'male',
+    address: 'Viale Manzoni 6',
+    imgUrl: 'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png'
+  }
+
+  public isDisabled = false;
+
+  public fireEvent(){
+    console.log('event')
+    this.isDisabled = !this.isDisabled
+  }
 }
